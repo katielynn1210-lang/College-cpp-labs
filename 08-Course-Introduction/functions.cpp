6fe1f4c588a48fac21d39357b1211377d4457f43
Katie Williams <katielynn1210@gmail.com>
2026-07-03 14:23:04 -0500

    Add files via upload

---
+++ 08-Course-Introduction/functions.cpp
@@ -0,0 +1,26 @@
+// functions.cpp
+
+#include <iostream>
+#include <iomanip>
+#include "functions.h"
+
+using namespace std;
+
+//function descriptions
+//prints welcome message
+void printWelcomeMessage() {
+    cout << "Welcome to the Computer Programming course! Enjoy your learning adventure!\n" << endl;
+
+}
+
+//prints course info
+void printCourseInfo() {
+    cout << "--------Course Information--------" << endl;
+    cout << "Course Name: \t\tComputer Programming (COSC 1336)" << endl;
+    cout << "Course Description: \tIn this course you will learn how to create various computer programs using C++\n" << endl;
+}
+
+//prints closing message
+void printClosingMessage() {
+    cout << "Thank you for visiting the Computer Programming course! Visit again soon!" << endl;
+}
---
+++ 08-Course-Introduction/functions.h
@@ -0,0 +1,12 @@
+// functions.h
+// Created by Katie on 2/14/2026.
+
+#ifndef INTRODUCTION_APP_FUNCTIONS_H
+#define INTRODUCTION_APP_FUNCTIONS_H
+
+//function prototypes
+void printWelcomeMessage();
+void printCourseInfo();
+void printClosingMessage();
+
+#endif //INTRODUCTION_APP_FUNCTIONS_H
